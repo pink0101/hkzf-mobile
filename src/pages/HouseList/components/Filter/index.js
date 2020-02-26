ab1cf7f5f9bcb9093e63dedbc7b52a6a9cd07271
lc <1259493451@qq.com>
2020-02-26 22:51:26 +0800

    filterMore组件完成

--- src/pages/HouseList/components/Filter/index.js
+++ src/pages/HouseList/components/Filter/index.js
@@ -5,6 +5,8 @@ import FilterPicker from '../FilterPicker'
 import FilterMore from '../FilterMore'
 
 import styles from './index.module.css'
+// 导入自定义的axios
+import { API } from '../../../../utils/api'
 
 // 标题高亮状态
 // true 表示高亮
@@ -13,6 +15,13 @@ const titleSelectedStatus = {
   mode:false,
   price:false,
   more:false
+}
+// FilterPicker 和 FilterMore 组件的选中值
+const selectedValues = {
+  area:['area','null'],
+  mode:['null'],
+  price:['null'],
+  more:[]
 }
 
 /* 
@@ -27,24 +36,79 @@ const titleSelectedStatus = {
   7 在 Filter 组件中，提供 onSave 方法，作为确定按钮的事件处理程序，逻辑同上。
 */
 
+/* 
+  获取当前筛选条件的数据：
+
+  1 在 Filter 组件中，发送请求，获取所有筛选条件数据。
+  2 将数据保存为状态：filtersData。
+  3 封装渲染 FilterPicker 的方法 renderFilterPicker。
+
+  4 在方法中，根据 openType 的类型，从 filtersData 中获取到需要的数据。
+  5 将数据通过 props 传递给 FilterPicker 组件。
+  6 FilterPicker 组件接收到数据后，将其作为 PickerView 组件的 data （数据源）。
+*/ 
+
 export default class Filter extends Component {
   state = {
     titleSelectedStatus,
     // 控制 FilterPicker 或 FilteMore 组件的展示或隐藏
-    openType:''
+    openType:'',
+    // 所有筛选条件数据
+    filtersData:{},
+    // 筛选条件的选中值
+    selectedValues
+  }
+
+  componentDidMount() {
+    this.getFiltersData()
+  }
+
+  // 封装获取所有所有筛选条件的方法
+  async getFiltersData() {
+    // 获取当前定位城市id
+    const { value } = JSON.parse(localStorage.getItem('hkzf_city'))
+    const res = await API.get(`/houses/condition?id=${value}`)
+    console.log('filtersData',res)
+    this.setState({
+      filtersData:res.data.body
+    })
   }
 
   // 点击标题菜单实现高亮
   // 注意 this 指向的问题
   onTitleClick = (type) => {
-    // prevState 上一个状态的 state
-    this.setState(prevState => {
+    const { titleSelectedStatus, selectedValues } = this.state
+    // 创建新的标题选中状态对象
+    const newTitleSelectedState = { ...titleSelectedStatus }
+
+    // 遍历标题选中状态对象
+    // Object.keys 返回值['area','mode','price','more']
+    Object.keys(titleSelectedStatus).forEach(item => {
+      // item 表示数组中的每一项， 此处，就是每个标题的 type 值
+      if(item === type){
+        // 点击当前标题
+        newTitleSelectedState[type] = true
+        return
+      }
+      // 其他标题
+      const selectedVal = selectedValues[item]
+      if(item === 'area' && (selectedVal.length !== 2 || selectedVal[0] !== 'area') ){
+        // 高亮
+        newTitleSelectedState[item] = true
+      }else if(item === 'mode' && selectedVal[0] !== 'null'){
+        newTitleSelectedState[item] = true
+      }else if(item === 'price' && selectedVal[0] !== 'null'){
+        newTitleSelectedState[item] = true
+      }else if (item === 'more'){
+        // 更多选择项 FilterMore 组件
+      }else {
+        newTitleSelectedState[item] = false
+      }
+    })
+
+    this.setState(() => {
       return {
-        titleSelectedStatus:{
-          // 获取当前对象中所有属性的值
-          ...prevState.titleSelectedStatus,
-          [type]:true
-        },
+        titleSelectedStatus:newTitleSelectedState,
         // 展示对话框
         openType: type
       }
@@ -58,10 +122,75 @@ export default class Filter extends Component {
     })
   }
   // 确定 (隐藏对话框)
-  onSave = () => {
+  onSave = (value,type) => {
     this.setState({
-      openType:''
+      openType:'',
+      selectedValues:{
+        ...this.state.selectedValues,
+        // 只更新当前 type 对应的选中值
+        [type]: value
+      }
     })
+  }
+
+  // 渲染 FilterPicker 组件的方法
+  renderFilterPicker() {
+    const { openType, filtersData:{area,subway, rentType, price},selectedValues } = this.state
+    if(openType !== 'area' && openType !== 'mode' && openType !== 'price'){
+      return null
+    }
+    // 根据 openType 来拿到当前筛选条件数据
+    let data = []
+    let cols = 3 // 列数
+    let defaultValue = selectedValues[openType]
+    switch(openType) {
+      case 'area': 
+        data = [area,subway]
+        cols = 3
+        break
+      case 'mode': 
+        data = rentType
+        cols = 1
+        break
+      case 'price': 
+        data = price
+        cols = 1
+        break
+      default:
+        break
+    }
+    return (
+      <FilterPicker 
+      key={openType}
+      onCancel={this.onCancel} 
+      onSave={this.onSave} 
+      data={data} 
+      cols={cols} 
+      type={openType} 
+      defaultValue={defaultValue}
+      />
+    )
+  }
+
+  /* 
+    1 封装 renderFilterMore 方法，渲染 FilterMore 组件。
+    2 从 filtersData 中，获取数据（roomType, oriented, floor, characteristic），通过 props 传递给 FilterMore 组件。
+    3 FilterMore 组件中，通过 props 获取到数据，分别将数据传递给 renderFilters 方法。
+    4 在 renderFilters 方法中，通过参数接收数据，遍历数据，渲染标签。
+  */
+
+  renderFilterMore() {
+    const { selectedValues,openType,filtersData:{roomType, oriented, floor, characteristic} } = this.state
+    if( openType !== 'more'){
+      return null
+    }
+    const data = {
+      roomType, oriented, floor, characteristic
+    }
+
+    const defaultValue = selectedValues.more
+
+    return <FilterMore data={data} type = {openType} onSave = {this.onSave} defaultValue={defaultValue} onCancel={this.onCancel}/>
   }
 
   render() {
@@ -79,11 +208,13 @@ export default class Filter extends Component {
 
           {/* 前三个菜单对应的内容： */}
           {
-            (openType === 'area' || openType === 'mode' || openType === 'price') ? <FilterPicker onCancel={this.onCancel} onSave={this.onSave}/> :null
+            this.renderFilterPicker()
           }
 
           {/* 最后一个菜单对应的内容： */}
-          {/* <FilterMore /> */}
+          {
+            this.renderFilterMore()
+          }
         </div>
       </div>
     )
--- src/pages/HouseList/components/FilterMore/index.js
+++ src/pages/HouseList/components/FilterMore/index.js
@@ -4,40 +4,104 @@ import FilterFooter from '../../../../components/FilterFooter'
 
 import styles from './index.module.css'
 
+/* 
+  获取选中值以及设置高亮：
+
+  1 在 state 中添加状态 selectedValues（表示选中项的值）。
+  2 给标签绑定单击事件，通过参数获取到当前项的 value。
+  3 判断 selectedValues 中是否包含当前项的 value 值。
+  4 如果不包含，就将当前项的 value 添加到 selectedValues 数组中。
+  5 如果包含，就从 selectedValues 数组中移除（使用数组的 splice 方法，根据索引号删除）。
+  6 在渲染标签时，判断 selectedValues 数组中，是否包含当前项的 value，包含，就添加高亮类。
+*/
+
+
 export default class FilterMore extends Component {
-  // 渲染标签
-  renderFilters() {
-    // 高亮类名： styles.tagActive
-    return (
-      <span className={[styles.tag, styles.tagActive].join(' ')}>东北</span>
-    )
+  state = {
+    selectedValues:this.props.defaultValue
   }
 
+  onTagClick(value) {
+    const { selectedValues } = this.state
+    // 创建一个新数组
+    const newSelectedValues = [...selectedValues]
+    if (selectedValues.indexOf(value) <= -1){
+      newSelectedValues.push(value)
+    }else{
+      // 点击的该选项  数组中已有
+      // 拿到索引号
+      const index = newSelectedValues.findIndex(item => item === value)
+      newSelectedValues.splice(index,1) // 删除 
+    }
+    this.setState({
+      selectedValues: newSelectedValues
+    })
+  }
+
+  // 渲染标签
+  renderFilters(data) {
+    const { selectedValues } = this.state
+    // 高亮类名： styles.tagActive
+    return data.map(item => {
+      const isSelected = selectedValues.indexOf(item.value) > -1
+      return(
+        <span className={[styles.tag, isSelected ? styles.tagActive : '' ].join(' ')} key={item.value} onClick={ () => this.onTagClick(item.value) } >{item.label}</span>
+      )
+    })
+  }
+
+  // 清除按钮的事件处理程序
+  onCancel = () => {
+    this.setState({
+      selectedValues:[]
+    })
+  }
+
+  // 确定按钮的事件处理程序
+  onOk = () => {
+    const { type, onSave } = this.props
+    // onSave 是父组件中的方法
+    onSave(this.state.selectedValues,type)
+  } 
+
   render() {
+    const { data:{ roomType, oriented, floor, characteristic } } = this.props
     return (
       <div className={styles.root}>
         {/* 遮罩层 */}
-        <div className={styles.mask} />
-
+        <div className={styles.mask} onClick={this.props.onCancel} />
         {/* 条件内容 */}
         <div className={styles.tags}>
           <dl className={styles.dl}>
             <dt className={styles.dt}>户型</dt>
-            <dd className={styles.dd}>{this.renderFilters()}</dd>
+            <dd className={styles.dd}>{this.renderFilters(roomType)}</dd>
 
             <dt className={styles.dt}>朝向</dt>
-            <dd className={styles.dd}>{this.renderFilters()}</dd>
+            <dd className={styles.dd}>{this.renderFilters(oriented)}</dd>
 
             <dt className={styles.dt}>楼层</dt>
-            <dd className={styles.dd}>{this.renderFilters()}</dd>
+            <dd className={styles.dd}>{this.renderFilters(floor)}</dd>
 
             <dt className={styles.dt}>房屋亮点</dt>
-            <dd className={styles.dd}>{this.renderFilters()}</dd>
+            <dd className={styles.dd}>{this.renderFilters(characteristic)}</dd>
           </dl>
         </div>
 
         {/* 底部按钮 */}
-        <FilterFooter className={styles.footer} />
+
+        {
+          /* 
+          底部按钮
+
+          1 设置 FilterFooter 组件的取消按钮文字为：清除。
+          2 点击取消按钮时，清空所有选中项的值（selectedValues: []）。
+          3 点击确定按钮时，将当前选中项的值和 type，传递给 Filter 父组件。
+          4 在 Filter 组件中的 onSave 方法中，接收传递过来的选中值，更新状态 selectedValues。
+
+          说明：type 和 onSave 都由父组件通过 props 传递给该组件。
+        */
+        }
+        <FilterFooter className={styles.footer} cancelText='清除' onCancel={this.onCancel} onOk={this.onOk}  />
       </div>
     )
   }
